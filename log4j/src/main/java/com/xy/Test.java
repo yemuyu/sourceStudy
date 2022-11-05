09c717a938ecf3666b2a25826852cbf49dd5d929
yemuyu <yemuyu240@163.com>
2022-11-05 20:27:00 +0800

    log4j测试

---
+++ log4j/src/main/java/com/xy/Test.java
@@ -0,0 +1,16 @@
+package com.xy;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author liufq
+ * @description:
+ * @date 2022/11/5
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Test.class);
+        logger.info("hello log");
+    }
+}
